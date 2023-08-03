eacd74f8976d0c5aee67d962657cd7594e2ff56a
EL Marsho <emarsho@gmail.com>
2023-08-02 22:35:25 -0500

    added SearchManufacturer.tsx

---
+++ components/SearchBar.tsx
@@ -0,0 +1,23 @@
+"use client";
+import {useState} from 'react'
+
+import {SearchManufacturer} from '@/components'
+
+const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState('')
+
+  const handleSearch = () => {};
+
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="search__bar-item">
+              <SearchManufacturer
+                  manufacturer={manufacturer}
+                  setManufacture={setManufacturer}
+              />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
